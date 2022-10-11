a029258999ece59de6244005276bf5d5d7cfeb68
Varun Sharma <62585746+varunsharma6956@users.noreply.github.com>
2022-10-11 10:41:56 +0530

    Create maximum_handshakes.java

---
+++ maximum_handshakes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+    
+    int num = 10;
+
+    int t = num * (num-1) / 2;
+
+		System.out.println("For "+ num +" people will be " + t +" handshakes");
+	}
+}
